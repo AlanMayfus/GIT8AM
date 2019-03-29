3149e0511fe7c1d898b521774fcbe378a862e680
AlanMayfus <sam.mayfus@gmail.com>
2019-03-29 09:42:44 +0530

    iii

--- src/com/test/git/Baseclass.java
+++ src/com/test/git/Baseclass.java
@@ -1,14 +1,30 @@
 package com.test.git;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
 public class Baseclass {
 
-	public static void main(String[] args) {
-		System.out.println("hello world");
-
-	}
-
-	public void newday()
+		public static void main(String[] args) {
+			Map <Integer,String>li=new LinkedHashMap<>();
+			li.put(10, "sql");
+			li.put(20, "java");
+			li.put(30, "csharp");
+			li.put(40, "spring");
+			li.put(20,  "c++");
+	System.out.println(li);
+	//iteration
+	Set<Entry<Integer,String>> a=li.entrySet();
+	for(Entry<Integer,String> b:a)
 	{
-		
+		System.out.println(b);
+		System.out.println();
+		System.out.println(b.getKey());
+		System.out.println(b.getValue());
+	}
+	
+
 	}
 }
