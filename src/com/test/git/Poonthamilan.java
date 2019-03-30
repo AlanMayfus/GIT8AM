e8d17a8a1ffb6dd412666120648320229a228093
poonthamilan0818 <poonthamilan0818@gmail.com>
2019-03-31 00:22:32 +0530

    my class

---
+++ src/com/test/git/Poonthamilan.java
@@ -0,0 +1,13 @@
+package com.test.git;
+
+public class Poonthamilan {
+	public static void main(String[] args) {
+		String str="cloaning has successful";
+		char charAt = str.charAt(6);
+		System.out.println(charAt);
+		int length = str.length();
+		System.out.println(length);
+		
+	}
+
+}
